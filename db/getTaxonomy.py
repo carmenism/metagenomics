c8291ee71a61e09e3e0847f29dc51b00a71df970
crstjean <crstjean@localhost>
2014-04-24 14:32:59 -0400

    added changes to taxonomy script but still totally untested

--- db/getTaxonomy.py
+++ db/getTaxonomy.py
@@ -1,6 +1,8 @@
 import dbDef
 import sqlite3
 
+abbrRanks = [“domain”, “kingdom”, “phylum”, “class”, “order”, “family”, “genus”, “species”]
+
 def getParentTaxidFromTaxid(cursor, taxid):
     sql = "SELECT " + dbDef.tblTaxonomy_col_parent_tax_id.name + " FROM " + dbDef.tblTaxonomy.name + " WHERE " + dbDef.tblTaxonomy_col_tax_id.name + " = '" + taxid + "';"
     
@@ -28,3 +30,26 @@ def getRankFromTaxid(cursor, taxid):
         return data
     except sqlite3.Error:
         return None
+
+def isPartOfAbbreviatedRank(rank):
+    return rank in abbrRanks
+
+def getAbbrTaxonomyFromTaxidRecursive(cursor, taxid, list):
+    if taxid == 1: # this is root
+        return
+
+    rank = getRankFromTaxid(cursor, taxid)
+    
+    if isPartOfAbbreviated(rank):
+        list.append((taxid, rank))
+
+    parentTaxid = getParentTaxidFromTaxid(cursor, taxid)
+        
+    getAbbrTaxonomyFromTaxidRecursive(cursor, parentTaxid, list)
+
+def getAbbrTaxonomyFromTaxid(cursor, taxid):
+    list = []
+
+    getAbbrTaxonomyFromTaxidRecursive(cursor, taxid, list)
+
+    return list
