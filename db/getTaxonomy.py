f2ffeb6a4d840800fa95528960aee4d19a47b7bd
Thomas Rossi <trn5@wildcats.unh.edu>
2014-04-24 15:23:30 -0400

    Made taxonomy script functional

--- db/getTaxonomy.py
+++ db/getTaxonomy.py
@@ -1,55 +1,73 @@
+#!/usr/bin/env python
 import dbDef
 import sqlite3
 
 abbrRanks = ["domain", "kingdom", "phylum", "class", "order", "family", "genus", "species"]
 
+def getSingleFieldFromSql(cursor, sql):
+    try:
+        cursor.execute(sql)
+        
+        data = str(cursor.fetchone()[0])
+        
+        return data
+    except sqlite3.Error:
+        return None
+
+def getNameFromTaxid(cursor, taxid):
+    sql = "SELECT " + dbDef.tblTaxonomy_col_tax_name.name + " FROM " + dbDef.tblTaxonomy.name + " WHERE " + dbDef.tblTaxonomy_col_tax_id.name + " = '" + taxid + "';"
+    
+    return getSingleFieldFromSql(cursor, sql)
+    
+
 def getParentTaxidFromTaxid(cursor, taxid):
     sql = "SELECT " + dbDef.tblTaxonomy_col_parent_tax_id.name + " FROM " + dbDef.tblTaxonomy.name + " WHERE " + dbDef.tblTaxonomy_col_tax_id.name + " = '" + taxid + "';"
-    
-    try:
-        cursor.execute(sql)
-        
-        data = cursor.fetchone()
-        
-        print data
-        
-        return data
-    except sqlite3.Error:
-        return None
+
+    return getSingleFieldFromSql(cursor, sql)
         
 def getRankFromTaxid(cursor, taxid):
-    sql = "SELECT "+ dbDef.tblTaxonomy_col_rank.name + " FROM " + dbDef.tblTaxonomy.name + " WHERE " + dbDef.tblTaxonomy_col_tax_id.name + " = " + "'" + taxid + "';"
+    sql = "SELECT "+ dbDef.tblTaxonomy_col_tax_rank.name + " FROM " + dbDef.tblTaxonomy.name + " WHERE " + dbDef.tblTaxonomy_col_tax_id.name + " = " + "'" + taxid + "';"
     
-    try:
-        cursor.execute(sql)
-        
-        data = cursor.fetchone()
-        
-        print data
-        
-        return data
-    except sqlite3.Error:
-        return None
+    return getSingleFieldFromSql(cursor, sql)
 
 def isPartOfAbbreviatedRank(rank):
-    return rank in abbrRanks
+    return rank.lower() in abbrRanks
 
 def getAbbrTaxonomyFromTaxidRecursive(cursor, taxid, taxList):
-    if taxid == 1: # this is root
-        return
+    if taxid == "1": # this is root
+        return True
 
     rank = getRankFromTaxid(cursor, taxid)
+    name = getNameFromTaxid(cursor, taxid)
     
-    if isPartOfAbbreviated(rank):
-        taxList.append((taxid, rank))
+    if rank is None:
+        return False
+    
+    if isPartOfAbbreviatedRank(rank):
+        taxList.insert(0, (taxid, name, rank))
 
     parentTaxid = getParentTaxidFromTaxid(cursor, taxid)
         
-    getAbbrTaxonomyFromTaxidRecursive(cursor, parentTaxid, taxList)
+    if parentTaxid is None:
+        return False
+        
+    return getAbbrTaxonomyFromTaxidRecursive(cursor, parentTaxid, taxList)
 
 def getAbbrTaxonomyFromTaxid(cursor, taxid):
     taxList = []
 
-    getAbbrTaxonomyFromTaxidRecursive(cursor, taxid, taxList)
+    completeAbbrTax = getAbbrTaxonomyFromTaxidRecursive(cursor, taxid, taxList)
 
-    return taxList
+    if completeAbbrTax:
+        return taxList
+    else:
+        return None
+
+conn = sqlite3.connect("Metagenomics.db")
+cursor = conn.cursor()
+print getParentTaxidFromTaxid(cursor,"58777")
+print getRankFromTaxid(cursor,"58777")
+print getNameFromTaxid(cursor,"58777")
+print getAbbrTaxonomyFromTaxid(cursor, "58777")
+conn.commit()
+conn.close()
