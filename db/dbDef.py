a67f08b11fce70d9f992c41f4219184a43d58494
crstjean <crstjean@localhost>
2014-05-11 17:31:08 -0400

    added documentation to dbDef

--- db/dbDef.py
+++ db/dbDef.py
@@ -1,23 +1,74 @@
 #!/usr/bin/env python
-# Filename: dbDef.py
+"""
+============
+dbDef module
+============
 
+Authors
+-------
+Thomas Rossi (trn5@unh.edu)
+Carmen St. Jean (crr8@unh.edu)
+    
+Date
+----
+April 15, 2014
+    
+Abstract
+--------
+This module defines table names and column names for the metagenomics database
+based on the reference file and the NCBI taxonomy.  
+"""
 import sqlite3
 
 class ColumnDefinition:
-    def __init__(self, col_name, col_type):
-        self.name = col_name
-        self.type = col_type
+    """
+    This class represents a definition for a column in an SQL table.  Stores
+    the name and type of the column.  
+    """
+    def __init__(self, colName, colType):
+        """
+        Creates a new column definition object.
+        
+        Arguments:
+            colName -- The name of the column.
+            colType -- The data type of the column.
+        """
+        self.name = colName
+        self.type = colType
         
     def createString(self):
+        """
+        Creates a string for the column definition to be used in a CREATE
+        TABLE SQLite statement.
+        """
         return self.name + " " + self.type
 
 class TableDefinition:
-    def __init__(self, table_name, table_cols, table_primary_key = None):
-        self.name = table_name
-        self.cols = table_cols
-        self.primary_key = table_primary_key
+    """
+    This class represents a definition for a table in an SQL database.  Stores
+    the name of the table and a list of column definition objects.
+    """
+    def __init__(self, tableName, tableCols, tablePrimaryKey = None):
+        """
+        Creates a new table definition object.
+        
+        Arguments:
+            tableName -- The name of the table.
+            tableCols -- A list of ColumnDefinition objects.
+            tablePrimaryKey -- An optional parameter for specifying the columns
+                which form the primary key.  If the primary key should be a
+                single column, then do not use this parameter and add "PRIMARY
+                KEY" to the name of the ColumnDefinition object.
+        """
+        self.name = tableName
+        self.cols = tableCols
+        self.primary_key = tablePrimaryKey
         
     def createString(self):
+        """
+        Builds a string that is an SQLite CREATE TABLE statement for this
+        table.
+        """
         colStr = ", ".join([col.createString() for col in self.cols])
         
         createStr = "CREATE TABLE " + self.name + " (" + colStr + " "
@@ -28,6 +79,9 @@ class TableDefinition:
         return createStr + ")"
     
     def dropString(self):
+        """
+        Builds a string that is an SQLite DROP TABLE statement for this table.
+        """
         return "DROP TABLE IF EXISTS " + self.name
     
 tblSpecies_col_ncid = ColumnDefinition("ncid", "VARCHAR(20) PRIMARY KEY")
@@ -53,11 +107,25 @@ tblTaxonomy_cols = [tblTaxonomy_col_tax_id, tblTaxonomy_col_tax_name, tblTaxonom
 tblTaxonomy = TableDefinition("tblTaxonomy", tblTaxonomy_cols)
 
 def dropTable(cursor, table):
+    """
+    Drops the specified table from the SQLite database.
+    
+    Arguments:
+        cursor -- A cursor pointing to the metagenomics SQLite database.
+        table -- A TableDefinition object.
+    """
     sql = table.dropString()
 
     cursor.execute(sql)
     
 def createTable(cursor, table):
+    """
+    Creates the specified table from the SQLite database.
+    
+    Arguments:
+        cursor -- A cursor pointing to the metagenomics SQLite database.
+        table -- A TableDefinition object.
+    """
     sql = table.createString()
 
     print sql
