5f890e8fea85769331e81423e8ccf065a1c8d601
crstjean <crstjean@localhost>
2014-05-11 17:07:37 -0400

    added comments to buildTaxonomy.py

--- db/buildTaxonomy.py
+++ db/buildTaxonomy.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
+"""
+====================
+buildTaxonomy module
+====================
+
+Authors
+-------
+Thomas Rossi (trn5@unh.edu)
+Carmen St. Jean (crr8@unh.edu)
+    
+Date
+----
+May 6, 2014
+    
+Abstract
+--------
+This module allows for easy retrieval of the abbreviated taxonomy for a
+taxonomy ID from the taxonomy database based on the NCBI taxonomy.
+"""
 import dbDef
 import sqlite3
 
 abbrRanks = ["superkingdom", "kingdom", "phylum", "class", "order", "family", "genus", "species"]
 
 def getSingleFieldFromSql(cursor, sql):
+    """
+    Executes an SQLite expression and tries to return a single result as a string.
+    
+    Arguments:
+        cursor -- A cursor pointing to the metagenomics SQLite database.
+        sql -- An SQLite expression to execute.
+    """
     try:
         cursor.execute(sql)
         
@@ -20,25 +46,62 @@ def getSingleFieldFromSql(cursor, sql):
         return None
 
 def getNameFromTaxid(cursor, taxid):
+    """
+    Returns the name which matches the given taxonomy ID.
+    
+    Arguments:
+        cursor -- A cursor pointing to the metagenomics SQLite database.
+        taxid -- The taxonomy ID for a species, genus, family, etc.
+    """
     sql = "SELECT " + dbDef.tblTaxonomy_col_tax_name.name + " FROM " + dbDef.tblTaxonomy.name + " WHERE " + dbDef.tblTaxonomy_col_tax_id.name + " = '" + taxid + "';"
     
     return getSingleFieldFromSql(cursor, sql)
-    
 
 def getParentTaxidFromTaxid(cursor, taxid):
+    """
+    Returns the taxonomy ID of the parent of the given taxonomy ID.
+    
+    Arguments:
+        cursor -- A cursor pointing to the metagenomics SQLite database.
+        taxid -- The taxonomy ID for a species, genus, family, etc.
+    """
     sql = "SELECT " + dbDef.tblTaxonomy_col_parent_tax_id.name + " FROM " + dbDef.tblTaxonomy.name + " WHERE " + dbDef.tblTaxonomy_col_tax_id.name + " = '" + taxid + "';"
 
     return getSingleFieldFromSql(cursor, sql)
         
 def getRankFromTaxid(cursor, taxid):
+    """
+    Returns the rank for the given taxonomy ID.
+    
+    Arguments:
+        cursor -- A cursor pointing to the metagenomics SQLite database.
+        taxid -- The taxonomy ID for a species, genus, family, etc.
+    """
     sql = "SELECT "+ dbDef.tblTaxonomy_col_tax_rank.name + " FROM " + dbDef.tblTaxonomy.name + " WHERE " + dbDef.tblTaxonomy_col_tax_id.name + " = " + "'" + taxid + "';"
     
     return getSingleFieldFromSql(cursor, sql)
 
 def isPartOfAbbreviatedRank(rank):
+    """
+    Returns true if the specified rank is part of the abbreviated ranks.
+    
+    Arguments:
+        rank -- A rank that describes a level of taxonomy (e.g., "species",
+            "subfamily", "superorder")
+    """
     return rank.lower() in abbrRanks
 
 def getAbbrTaxonomyFromTaxidRecursive(cursor, taxid, taxList):
+    """
+    Gets the abbreviated taxonomy for a taxonomy ID.  This is a recursive
+    helper function which should not be called directly.  Call the
+    getAbbrTaxonomyFromTaxid method instead.
+    
+    Arguments:
+        cursor -- A cursor pointing to the metagenomics SQLite database.
+        taxid -- The taxonomy ID for a species, genus, family, etc.
+        taxList -- A list where the abbreviated taxonomy is added.
+    """
     if taxid == "1": # this is root
         return True
 
@@ -61,6 +124,16 @@ def getAbbrTaxonomyFromTaxidRecursive(cursor, taxid, taxList):
     return getAbbrTaxonomyFromTaxidRecursive(cursor, parentTaxid, taxList)
 
 def getAbbrTaxonomyFromTaxid(cursor, taxid):
+    """
+    Gets the abbreviated taxonomy for a taxonomy ID.  Returns the results as a
+    list where each item is a tuple of (taxonomy ID, name, rank).  The items in
+    the list are ordered from highest (i.e., kingdom) to lowest (i.e., species)
+    rank.
+    
+    Arguments:
+        cursor -- A cursor pointing to the metagenomics SQLite database.
+        taxid -- The taxonomy ID for a species, genus, family, etc.
+    """
     taxList = []
 
     if cursor is None:
