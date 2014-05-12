549af57986572b4b374eb690dee1a559118e303f
crstjean <crstjean@localhost>
2014-05-11 21:07:04 -0400

    added comments to runfastqc

--- runfastqc.py
+++ runfastqc.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
+"""
+=========
+runfastqc
+=========
 
+Authors
+-------
+Carmen St. Jean (crr8@unh.edu)
+    
+Date
+----
+April 1, 2014
+
+Abstract
+--------
+Runs fastqc for all of the files in the specified directory.
+
+Instructions
+------------
+On a computer which has python, call:
+
+    python runfastqc.py /path/to/fastq/files/
+"""
 import sys
 import glob
 import os
@@ -14,4 +36,4 @@ for subdir in os.walk(directory):
         if (not "adnq" in fileName):
             command = "fastqc " + fileName + " "
             print "Running fastqc on " + fileName
-            #subprocess.call(command.split())
+            subprocess.call(command.split())
