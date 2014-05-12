a5df06b8fbd9364bebdcf7d3c7b6298d7190e153
crstjean <crstjean@localhost>
2014-05-11 20:57:38 -0400

    added documentation to rankComparison

--- db/rankComparison.py
+++ db/rankComparison.py
@@ -1,11 +1,50 @@
 #!/usr/bin/env python
-################################################################################
-#rankComparison.py
-################################################################################
+"""
+======================
+tabulateResults module
+======================
+
+Authors
+-------
+Thomas Rossi (trn5@unh.edu)
+Carmen St. Jean (crr8@unh.edu)
+    
+Date
+----
+April 28, 2014
+    
+Abstract
+--------
+This module finds the lowest common ancestor between a list of species.  This
+code did not end up in our final pipeline.
+
+Instructions
+------------
+To run this file, you first need to make sure the taxonomy table is populated
+by running ncbiToDb.py on a computer which has python installed.
+
+Next, run this script on the command line of a computer where Python is
+installed with a command line argument specifying the BWA output, e.g.:
+
+    ./rankComparison.py 
+    
+If you have just added this file to your computer and this doesn't work,
+then it may be necessary to change the permissions.  On Linux:
+
+    chmod u+x rankComparison.py
+    
+It is also necessary to have buildTaxonomy.py in the same directory.
+"""
 import buildTaxonomy
 import sqlite3
 
 def getCommonAncestor(speciesList):
+    """
+    Returns the lowest common ancestor between the specified species.
+    
+    Arguments:
+        speciesList -- A list of species taxonomies.
+    """
     level = 0
     keepLooking = True
     
