110745c3cf1d42b4bff556e82e5903bb831d7a6d
crstjean <crstjean@localhost>
2014-05-11 20:58:18 -0400

    added documentation to rankComparison

--- db/rankComparison.py
+++ db/rankComparison.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """
-======================
-tabulateResults module
-======================
+=====================
+rankComparison module
+=====================
 
 Authors
 -------
